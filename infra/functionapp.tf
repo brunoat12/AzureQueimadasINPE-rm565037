3e4a8baa4d19d38518e89891568985d0b50c63be
brunoat12 <brunoat12@users.noreply.github.com>
2026-09-03 23:49:47 +0000

    Ajusta Function App para plano Consumption

--- infra/functionapp.tf
+++ infra/functionapp.tf
@@ -11,7 +11,7 @@ resource "azurerm_service_plan" "func_plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "Y1"
 }
 
 resource "azurerm_linux_function_app" "func" {
